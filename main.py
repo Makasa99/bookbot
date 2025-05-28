8b769d3ae50c7b4528f1d326fb4eb4223a2b0727
makasa99 <makasa99@gmail.com>
2025-05-28 18:29:12 +0200

    added character count and removed hard coded filepaths

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
+import sys
+
+if len(sys.argv) != 2:
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
+
 def get_book_text(filepath):
     with open(filepath) as f:
         return f.read()
 
 def main(get_book_text):
-    print(get_book_text("books/frankenstein.txt"))
+    print(get_book_text(sys.argv[1]))
 
 main(get_book_text)
-
+print("============ BOOKBOT ============")
+print(f"Analyzing book found at books/frankenstein.txt...")
+print("----------- Word Count ----------")
 from stats import word_count
-from stats import letter_count
+print("--------- Character Count -------")
+from stats import sort_letter_count
+print("============= END ===============")
--- stats.py
+++ stats.py
@@ -1,21 +1,42 @@
+import sys
+
 def word_count():
-    with open("books/frankenstein.txt") as f:
+    with open(sys.argv[1]) as f:
         text = f.read()
         words = text.split()
         total_words = len(words)
-        print(f"{total_words} words found in the document")
+        print(f"Found {total_words} total words")
 
 word_count()
 
+letters = {}
+
 def letter_count():
-    with open("books/frankenstein.txt") as f:
+    with open(sys.argv[1]) as f:
         text = f.read().lower()
-        letters = {}
+        #letters = {}
         for i in text:
             if i in letters:
                 letters[i] += 1
             else:
                 letters[i] = 1
-        print(letters)
+        #print(letters)
+        return letters
 
 letter_count()
+
+def sort_on(dict):
+    return dict["num"]
+
+def sort_letter_count():
+    sorted_letters = []
+    for i in letters:
+        if i.isalpha():
+            sorted_letters.append({"char": i, "num": letters[i]})
+            sorted_letters.sort(reverse=True, key=sort_on)
+    for i in sorted_letters:
+        print(f"{i["char"]}: {i["num"]}")
+
+
+
+sort_letter_count()
